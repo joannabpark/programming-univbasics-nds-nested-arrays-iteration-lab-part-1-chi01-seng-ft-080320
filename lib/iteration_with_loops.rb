b945e41a80b1bb65777c38227d49638d803ab9bc
JoAnna Park <joannabpark1@gmail.com>
2020-07-17 20:01:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,6 @@ while row_index < src.count do
   while element_dex < src[row_index].count do
 if src[row_index][element_index].even?
   p src[row_index][element_index]
-end
 element_index += 1
 end
 row_index += 1
